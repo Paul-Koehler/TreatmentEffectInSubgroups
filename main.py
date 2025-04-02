3813cfd34edd8e3686bb1c85a90f328a063b04c6
Paul Koehler <paul.b.koehler@gmail.com>
2025-04-03 01:01:48 +0200

    refactored functions into new file to avoid naming conflict and function names are now correctly written in lower case letters

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Generic ML imports
 from typing import List
 import gc
 
@@ -9,7 +8,6 @@ import pandas as pd
 import shap
 from econml._cate_estimator import BaseCateEstimator
 from econml.cate_interpreter import SingleTreeCateInterpreter
-# EconML imports
 from econml.dml import CausalForestDML, LinearDML, NonParamDML, DML
 from econml.inference._inference import InferenceResults
 from econml.orf import DMLOrthoForest
@@ -21,8 +19,21 @@ from sklearn.naive_bayes import BernoulliNB
 from sklearn.neural_network import MLPRegressor, MLPClassifier
 from sklearn.preprocessing import LabelEncoder
 from numpy.typing import NDArray
+from training_basic_eval import *
 
 print("Importing complete")
+
+def show_overview_data(data):
+    print(data.describe())
+    print(data.head(5))
+    corr_matrix = data.drop(columns=["Adj"]).corr()
+    plt.figure(figsize=(10, 8))
+    plt.imshow(corr_matrix, cmap='RdYlGn', interpolation='nearest')
+    plt.colorbar()
+    plt.xticks(range(len(corr_matrix.columns)), corr_matrix.columns, rotation=90)
+    plt.yticks(range(len(corr_matrix.columns)), corr_matrix.columns)
+    plt.title('Correlation Matrix')
+    plt.show()
 
 def plot_gini_curve(element_loss):
     element_loss = np.sort(element_loss)
@@ -41,166 +52,74 @@ def plot_gini_curve(element_loss):
     plt.grid()
     plt.show()
 
-
-def train_and_interpret(model, Y_train, T_train, X_train, W_train):
-    model.fit(Y_train, T_train, X=X_train, W=W_train)
-
-    intrp = SingleTreeCateInterpreter(include_model_uncertainty=True, max_depth=3, min_samples_leaf=10)
-    intrp.interpret(model, X_train)
-    plt.figure(figsize=(12, 8), dpi=400)
-    intrp.plot(feature_names=X_data.columns)
-    plt.title(model.__class__.__name__)
-    plt.show()
-
-def test_model(model, Y_test, T_test, X_data_test, W_test):
-    print(model.score(Y_test, T_test, X=X_data_test, W=W_test))
-
-def build_biomarker_effects(model, columns_to_analyze):
-    effects = []
-    effect_stds = []
-    for column in columns_to_analyze:
-        # Create test data with modified column
-        X_modified = X_data_test.copy()
-        if column != 'None':
-            X_modified[column] = 1
-
-        # Calculate effect between treatment 1 and treatment 0 for all samples
-        effect = model.effect(X_modified, T0=0, T1=1)
-        effects.append(np.mean(effect))
-        effect_stds.append(np.std(effect))
-    # Create forest plot
-    plt.figure(figsize=(10, 6))
-    y_pos = np.arange(len(columns_to_analyze))
-    plt.errorbar(effects, y_pos, xerr=effect_stds, fmt='o', capsize=5)
-    plt.yticks(y_pos, columns_to_analyze)
-    plt.xlabel(f'Difference in Treatment Effect (more means {label_encoder.inverse_transform([1])} is better)')
-    plt.title(f'Forest Plot of Treatment Effects for {model.__class__.__name__}')
-    plt.axvline(x=0, color='gray', linestyle='--')
-    plt.grid(True, axis='x')
-    plt.show()
-
-def build_biomarker_effects_with_intervals(model: BaseCateEstimator, columns_to_analyze):
-    effects = []
-    effect_lb = []
-    effect_ub = []
-    for column in columns_to_analyze:
-        # Create test data with modified column
-        X_modified = X_data_test.copy()
-        if column != 'None':
-            X_modified[column] = 1
-        effect: InferenceResults = model.effect_inference(X_modified)
-        point_est = np.mean(effect.point_estimate)
-        lb = np.mean(effect.conf_int(0.05)[0])
-        ub = np.mean(effect.conf_int(0.05)[1])
-        effects.append(point_est)
-        effect_lb.append(lb)
-        effect_ub.append(ub)
-
-    effect_lb = np.array(effect_lb)
-    effect_ub = np.array(effect_ub)
-    effects = np.array(effects)
-    plt.figure(figsize=(10, 6))
-    y_pos = np.arange(len(columns_to_analyze))
-    plt.errorbar(effects, y_pos, xerr=[effects - effect_lb, effect_ub - effects], fmt='o', capsize=5)
-    plt.yticks(y_pos, columns_to_analyze)
-    plt.xlabel(f'Difference in Treatment Effect (more means {label_encoder.inverse_transform([1])} is better)')
-    plt.title(f'Forest Plot of Treatment Effects for {model.__class__.__name__}')
-    plt.axvline(x=0, color='gray', linestyle='--')
-    plt.grid(True, axis='x')
-    plt.show()
-
-def show_shap_plot(model, X_data):
-    shap_values = model.shap_values(X_data[:], feature_names=X_data.columns, treatment_names=["gefitinib"], output_names=[Y_col])
-    ax = shap.plots.beeswarm(shap_values[Y_col]["gefitinib"], show=False)
-    plt.title(f"{Y_col} with {model.__class__.__name__}")
-    plt.tight_layout()
-    plt.show()
-
 def prepare_data(data, y_col, t_col, w_cols, drop_cols):
-    T = data[t_col]
-    Y = data[y_col]
-    W = data[w_cols]
-    X_data = data.drop(columns=[t_col, y_col] + w_cols + drop_cols)
+    t = data[t_col]
+    y = data[y_col]
+    w = data[w_cols]
+    x_data = data.drop(columns=[t_col, y_col] + w_cols + drop_cols)
     if not w_cols:
-        W = None
-    return X_data, Y, T, W
+        w = None
+    return x_data, y, t, w
 
-def show_overview_data(data):
-    print(data.describe())
-    print(data.head(5))
-    corr_matrix = data.drop(columns=["Adj"]).corr()
-    plt.figure(figsize=(10, 8))
-    plt.imshow(corr_matrix, cmap='RdYlGn', interpolation='nearest')
-    plt.colorbar()
-    plt.xticks(range(len(corr_matrix.columns)), corr_matrix.columns, rotation=90)
-    plt.yticks(range(len(corr_matrix.columns)), corr_matrix.columns)
-    plt.title('Correlation Matrix')
-    plt.show()
-
-def total_model_evaluation_and_training(model_class, model_params, X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test, interval_available=False, tree_explainer_available=False, shap_available=False, score_available=False):
+def total_model_evaluation_and_training(model_class, model_params, x_train, y_train, t_train, w_train, x_test, y_test, t_test, w_test, t_encoder, interval_available=False, tree_explainer_available=False, shap_available=False, score_available=False):
     model = model_class(**model_params)
     print(f"{model.__class__.__name__} evaluating")
     print(f"with params: {model_params['model_y'].__class__.__name__} and {model_params['model_t'].__class__.__name__}")
 
     if tree_explainer_available:
-        train_and_interpret(model, Y_train, T_train, X_data_train, W_train)
+        train_and_interpret(model, y_train, t_train, x_train, w_train)
     else:
         model.fit(Y_train, T_train, X=X_data_train, W=W_train)
-        print(f"Model with {model.__class__.__name__} scored {model.score(Y_test, T_test, X=X_data_test, W=W_test)}")
+        print(f"Model with {model.__class__.__name__} scored {model.score(y_test, t_test, X=x_test, W=w_test)}")
     if interval_available:
-        build_biomarker_effects_with_intervals(model, columns_to_analyze)
+        build_biomarker_effects_with_intervals(model, t_encoder, x_test, columns_to_analyze)
     else:
-        build_biomarker_effects(model, columns_to_analyze)
+        build_biomarker_effects(model, t_encoder, x_test, columns_to_analyze)
     if shap_available:
-        show_shap_plot(model, X_data)
+        show_shap_plot(model, x_train, Y_col)
 
     return model
 
 def try_k_fold(data):
-    global Y_train, T_train, W_train, Y_test, T_test, W_test
     k_fold = KFold(random_state=RANDOM_STATE, n_splits=CV_value, shuffle=True)
     split_generator = k_fold.split(data) # inlining this caused AccessViolation
-    for data_train_idxs, data_test_idxs in split_generator:
+    for data_train_idxes, data_test_idxes in split_generator:
         data_iter = data.copy()
-        X_train, Y_train, T_train, W_train = prepare_data(data_iter.iloc[data_train_idxs], Y_col, "Adj",
+        x_train, y_train, t_train, w_train = prepare_data(data_iter.iloc[data_train_idxes], Y_col, "Adj",
                                                           ["Age", "Sex", "Smoking_history", "Clinical_stage",
                                                            "N_stage"],
                                                           ["PatientID", "OS", "DFS", "OS_status", "DFS_status"])
-        X_test, Y_test, T_test, W_test = prepare_data(data_iter.iloc[data_test_idxs], Y_col, "Adj",
+        x_test, y_test, t_test, w_test = prepare_data(data_iter.iloc[data_test_idxes], Y_col, "Adj",
                                                       ["Age", "Sex", "Smoking_history", "Clinical_stage", "N_stage"],
                                                       ["PatientID", "OS", "DFS", "OS_status", "DFS_status"])
 
-        T_test = label_encoder.transform(T_test)
-        T_train = label_encoder.transform(T_train)
+        t_test = label_encoder.transform(t_test)
+        t_train = label_encoder.transform(t_train)
 
         model1 = total_model_evaluation_and_training(
             CausalForestDML, {"model_y": MLPRegressor(), "model_t": MLPClassifier(), "discrete_treatment": True,
                               "random_state": RANDOM_STATE, "cv": CV_value, "verbose": 5},
-            X_train, Y_train, T_train, W_train, X_test, Y_test, T_test, W_test,
+            x_train, y_train, t_train, w_train, x_test, y_test, t_test, w_test, label_encoder,
             interval_available=True, tree_explainer_available=True, shap_available=True, score_available=True)
     print("Finished Model1 with CV")
 
-def load_data():
+def load_data(encoder):
     data = pd.read_csv("data/individualPatients.csv")
-    X_data, Y, T, W = prepare_data(data, Y_col, "Adj", ["Age", "Sex", "Smoking_history", "Clinical_stage", "N_stage"],
+    x_data, y, t, w = prepare_data(data, Y_col, "Adj", ["Age", "Sex", "Smoking_history", "Clinical_stage", "N_stage"],
                                    ["PatientID", "OS", "DFS", "OS_status", "DFS_status"])
-    Y = Y.values
-    global label_encoder
-    label_encoder = LabelEncoder()
-    T = label_encoder.fit_transform(T)
-    X_data_train, X_data_test = X_data, X_data
-    Y_train, Y_test = Y, Y
-    T_train, T_test = T, T
-    W_train, W_test = W, W
-    if W is not None:
-        X_data_train, X_data_test, Y_train, Y_test, T_train, T_test, W_train, W_test = train_test_split(X_data, Y, T, W,
-                                                                                                        test_size=0.2,
-                                                                                                        random_state=RANDOM_STATE)
+    y = y.values
+    t = encoder.fit_transform(t)
+    x_data_train, x_data_test = x_data, x_data
+    y_train, y_test = y, y
+    t_train, t_test = t, t
+    w_train, w_test = w, w
+    if w is None:
+        w_train, w_test = train_test_split(w, test_size=0.2, random_state=RANDOM_STATE)
     else:
-        X_data_train, X_data_test, Y_train, Y_test, T_train, T_test = train_test_split(X_data, Y, T, test_size=0.2,
+        x_data_train, x_data_test, y_train, y_test, t_train, t_test = train_test_split(x_data, y, t, test_size=0.2,
                                                                                        random_state=RANDOM_STATE)
-        W_train, W_test = None, None
-    return data, X_data, Y, T, W, X_data_train, X_data_test, Y_train, Y_test, T_train, T_test, W_train, W_test, label_encoder
+        w_train, w_test = None, None
+    return data, x_data, y, t, w, x_data_train, x_data_test, y_train, y_test, t_train, t_test, w_train, w_test
 
 RANDOM_STATE = 15
 CV_value= 6
@@ -210,51 +129,54 @@ Y_col = "DFS"
 
 if __name__ == "__main__":
     columns_to_analyze = ['EGFR_subtype', 'NKX2_1_Gain', 'CDKN2A_Loss', 'PIK3CA', 'TERT_Gain', 'CDK4_Gain', 'STK11_Loss', 'RB1', 'None']
-    data, X_data, Y, T, W, X_data_train, X_data_test, Y_train, Y_test, T_train, T_test, W_train, W_test, label_encoder = load_data()
-    show_overview_data(data)
-    try_k_fold(data)
-    data, X_data, Y, T, W, X_data_train, X_data_test, Y_train, Y_test, T_train, T_test, W_train, W_test, label_encoder = load_data()
+    label_encoder = LabelEncoder()
+    dataframe, X_data, Y, T, W, X_data_train, X_data_test, Y_train, Y_test, T_train, T_test, W_train, W_test = load_data(label_encoder)
+    show_overview_data(dataframe)
+    try_k_fold(dataframe)
 
     total_model_evaluation_and_training(
         CausalForestDML, {"model_y": lgb.LGBMRegressor(), "model_t": lgb.LGBMClassifier(), "discrete_treatment": True, "random_state": RANDOM_STATE, "cv": CV_value, "verbose": 1},
-        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test,
+        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test, label_encoder,
         interval_available=True, tree_explainer_available=True, shap_available=True, score_available=True)
     print("Finished Model1")
 
     total_model_evaluation_and_training(
         LinearDML, {"model_y": lgb.LGBMRegressor(), "model_t": lgb.LGBMClassifier(), "discrete_treatment": True, "random_state": RANDOM_STATE, "cv": CV_value},
-        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test,
+        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test, label_encoder,
         interval_available=True, tree_explainer_available=True, shap_available=True, score_available=True)
     print("Finished Model2")
 
     total_model_evaluation_and_training(
         LinearDML, {"model_y": "automl", "model_t": "automl", "discrete_treatment": True, "random_state": RANDOM_STATE, "cv": CV_value},
-        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test,
+        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test, label_encoder,
         interval_available=True, tree_explainer_available=True, shap_available=True, score_available=True)
     print("Finished Model3")
 
     total_model_evaluation_and_training(
         CausalForestDML, {"model_y": MLPRegressor(), "model_t": MLPClassifier(), "discrete_treatment": True, "random_state": RANDOM_STATE, "cv": CV_value},
-        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test,
+        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test, label_encoder,
         interval_available=True, tree_explainer_available=True, shap_available=True, score_available=True)
     print("Finished Model4")
 
     #High loss
     # No model5 because no interval
-    total_model_evaluation_and_training(NonParamDML, {"model_y": RandomForestRegressor(), "model_t": RandomForestClassifier(), "model_final": AdaBoostRegressor(), "discrete_treatment": True, "random_state": RANDOM_STATE, "cv": CV_value}, X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test, interval_available=False, tree_explainer_available=False, shap_available=True, score_available=True)
+    total_model_evaluation_and_training(
+        NonParamDML, {"model_y": RandomForestRegressor(), "model_t": RandomForestClassifier(), "model_final": AdaBoostRegressor(), "discrete_treatment": True, "random_state": RANDOM_STATE, "cv": CV_value},
+        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test, label_encoder,
+        interval_available=False, tree_explainer_available=False, shap_available=True, score_available=True)
     print("Finished Model5")
 
     #show_shap_plot(model8, X_data) takes too long, different explainer+
     total_model_evaluation_and_training(
         DMLOrthoForest, {"model_T": BernoulliNB(), "model_Y": AdaBoostRegressor(), "model_T_final": AdaBoostRegressor(), "model_Y_final": AdaBoostRegressor(), "random_state": RANDOM_STATE},
-        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test,
+        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test, label_encoder,
         interval_available=True, tree_explainer_available=True, shap_available=False, score_available=False)
     print("Finished Model8")
 
     total_model_evaluation_and_training(
         DML, {"model_t": BernoulliNB(), "model_y": AdaBoostRegressor(), "model_final": ElasticNet(), "random_state": RANDOM_STATE, "discrete_treatment": True},
-                                        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test,
-                                        interval_available=False, tree_explainer_available=False, shap_available=True, score_available=True)
+        X_data_train, Y_train, T_train, W_train, X_data_test, Y_test, T_test, W_test, label_encoder,
+        interval_available=False, tree_explainer_available=False, shap_available=True, score_available=True)
     print("Finished Model9")
 
 
---
+++ training_basic_eval.py
@@ -0,0 +1,99 @@
+from typing import List
+import gc
+
+import matplotlib.pyplot as plt
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
+import shap
+from econml._cate_estimator import BaseCateEstimator
+from econml.cate_interpreter import SingleTreeCateInterpreter
+from econml.dml import CausalForestDML, LinearDML, NonParamDML, DML
+from econml.inference._inference import InferenceResults
+from econml.orf import DMLOrthoForest
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier, AdaBoostRegressor
+from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.neural_network import MLPRegressor, MLPClassifier
+from sklearn.preprocessing import LabelEncoder
+from numpy.typing import NDArray
+
+def test_model(model, Y_test, T_test, X_data_test, W_test):
+    print(model.score(Y_test, T_test, X=X_data_test, W=W_test))
+
+def build_biomarker_effects(model, t_encoder, x_test, columns_to_analyze):
+    effects = []
+    effect_stds = []
+    for column in columns_to_analyze:
+        # Create test dataframe with modified column
+        X_modified = x_test.copy()
+        if column != 'None':
+            X_modified[column] = 1
+
+        # Calculate effect between treatment 1 and treatment 0 for all samples
+        effect = model.effect(X_modified, T0=0, T1=1)
+        effects.append(np.mean(effect))
+        effect_stds.append(np.std(effect))
+    # Create forest plot
+    plt.figure(figsize=(10, 6))
+    y_pos = np.arange(len(columns_to_analyze))
+    plt.errorbar(effects, y_pos, xerr=effect_stds, fmt='o', capsize=5)
+    plt.yticks(y_pos, columns_to_analyze)
+    plt.xlabel(f'Difference in Treatment Effect (more means {t_encoder.inverse_transform([1])} is better)')
+    plt.title(f'Forest Plot of Treatment Effects for {model.__class__.__name__}')
+    plt.axvline(x=0, color='gray', linestyle='--')
+    plt.grid(True, axis='x')
+    plt.show()
+
+def train_encoder_for_treatment(encoder, T_train):
+    encoder.fit(T_train)
+    return encoder
+
+def build_biomarker_effects_with_intervals(model: BaseCateEstimator, t_encoder, x_test, columns_to_analyze):
+    effects = []
+    effect_lb = []
+    effect_ub = []
+    for column in columns_to_analyze:
+        # Create test dataframe with modified column
+        X_modified = x_test.copy()
+        if column != 'None':
+            X_modified[column] = 1
+        effect: InferenceResults = model.effect_inference(X_modified)
+        point_est = np.mean(effect.point_estimate)
+        lb = np.mean(effect.conf_int(0.05)[0])
+        ub = np.mean(effect.conf_int(0.05)[1])
+        effects.append(point_est)
+        effect_lb.append(lb)
+        effect_ub.append(ub)
+
+    effect_lb = np.array(effect_lb)
+    effect_ub = np.array(effect_ub)
+    effects = np.array(effects)
+    plt.figure(figsize=(10, 6))
+    y_pos = np.arange(len(columns_to_analyze))
+    plt.errorbar(effects, y_pos, xerr=[effects - effect_lb, effect_ub - effects], fmt='o', capsize=5)
+    plt.yticks(y_pos, columns_to_analyze)
+    plt.xlabel(f'Difference in Treatment Effect (more means {t_encoder.inverse_transform([1])} is better)')
+    plt.title(f'Forest Plot of Treatment Effects for {model.__class__.__name__}')
+    plt.axvline(x=0, color='gray', linestyle='--')
+    plt.grid(True, axis='x')
+    plt.show()
+
+def train_and_interpret(model, y_train, t_train, x_train, w_train, *, feature_names=None):
+    model.fit(y_train, t_train, X=x_train, W=w_train)
+
+    intrp = SingleTreeCateInterpreter(include_model_uncertainty=True, max_depth=3, min_samples_leaf=10)
+    intrp.interpret(model, x_train)
+    plt.figure(figsize=(12, 8), dpi=400)
+    intrp.plot(feature_names=feature_names)
+    plt.title(model.__class__.__name__)
+    plt.show()
+
+def show_shap_plot(model, x_data, Y_col_name):
+    shap_values = model.shap_values(x_data[:], feature_names=x_data.columns, treatment_names=["gefitinib"], output_names=[Y_col_name])
+    ax = shap.plots.beeswarm(shap_values[Y_col_name]["gefitinib"], show=False)
+    plt.title(f"{Y_col_name} with {model.__class__.__name__}")
+    plt.tight_layout()
+    plt.show()
